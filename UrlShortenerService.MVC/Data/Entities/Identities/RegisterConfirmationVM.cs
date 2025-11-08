b9d65e598ea531fef00e09775e54ebec8b5971d9
Voanh4869 <votuananh0925@gmail.com>
2025-11-08 20:53:09 +0700

    Add files via upload

---
+++ UrlShortenerService.MVC/Data/Entities/Identities/RegisterConfirmationVM.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortenerService.MVC.ViewModels.Identities
+{
+    [Bind("Email, ReturnUrl, Code, UserId")]
+    public class RegisterConfirmationVM
+    {
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public bool DisplayConfirmAccountLink { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string? EmailConfirmationUrl { get; set; }
+
+        public string ReturnUrl { get; set; } = "~/";
+        public string? Code { get; set; }
+        public string? UserId { get; set; }
+
+    }
+}
