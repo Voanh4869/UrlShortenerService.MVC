b373f20727e10e63ed151ded68e1caf18b644789
Light Celline <166616850+lightceline@users.noreply.github.com>
2025-11-08 18:28:16 +0700

    Add files via upload

---
+++ UrlShortenerService.MVC/Common/MaxLengthConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortenerService.MVC.Common
+{
+    public class MaxLengthConfig
+    {
+        public const int ORIGIN_URL = 2048;
+        public const int SHORTENED_URL = 200;
+    }
+}
