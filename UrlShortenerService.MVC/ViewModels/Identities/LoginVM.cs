07da05445e4e363a6d6c0f255b84ab30e81a7e01
Voanh4869 <votuananh0925@gmail.com>
2025-11-08 20:48:52 +0700

    Add files via upload

---
+++ UrlShortenerService.MVC/ViewModels/Identities/LoginVM.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortenerService.MVC.ViewModels.Identities
+{
+    [Bind("Email, Password, RememberMe, ReturnUrl")]
+    public class LoginVM
+    {
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+
+        public string ReturnUrl { get; set; } = "~/";
+
+        public IList<AuthenticationScheme>? ExternalLogins { get; set; }
+    }
+}
